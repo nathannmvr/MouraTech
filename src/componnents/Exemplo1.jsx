fc4b06b73a7d770e80c654e0146f70ce4a2d88f5
Nathan Maciel <nathannmvr@gmail.com>
2023-03-08 21:54:48 -0300

    1

--- src/componnents/Exemplo1.jsx
+++ src/componnents/Exemplo1.jsx
@@ -1,24 +1,33 @@
 import React from "react";
-import reactDOM from "react-dom/client";
+import ReactDOM from "react-dom/client";
 
 class Timer extends React.Component {
+
     constructor(props) {
         super(props);
-        this.state = {
+        this.state = { 
             seconds: this.props.start,
-        }
+        };
     }
 
     contar() {
         this.setState(
-            state => ({seconds: state.seconds +1})
-        );
+            state => ({seconds: state.seconds + 1})
+            );
     }
 
-    render() {
-        return <h1>Timer</h1>;
+    componentDidMount() {
+        this.interval = setInterval(() => this.contar(), this.props.ms);
+    }
+
+    componentWillUnmount() {
+        clearInterval(this.interval);
+    }
+
+    render(){
+        return <h1>Segundos: {this.state.seconds}</h1>;
     }
 
 }
 
-export default Timer();
+export default Timer;
--- src/index.jsx
+++ src/index.jsx
@@ -1,11 +1,34 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import Timer from './componnents/Exemplo1'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import Timer from "./components/Exemplo1";
+
+/* class OlaMundo extends React.Component{
+    render(){
+        return <h1>Olá {this.props.nome}</h1>;
+    }
+} */
+
+function OlaMundo(props){
+    return <h1>Olá {props.nome}</h1>;
+}
+
+class Pagina01 extends React.Component{
+    render(){
+        return (
+            <div>
+                <OlaMundo nome={this.props.nome[0]}></OlaMundo>
+                <OlaMundo nome={this.props.nome[1]}></OlaMundo>
+            </div> 
+        );
+    }
+}
+
+
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <div>
-    <Timer start={0}></Timer>
-    <Timer start={100}></Timer>
-  </div>
+    <div>
+        <Timer start={0} ms={1000}></Timer>
+        <Timer start={100} ms={500}></Timer>
+    </div>
 );
