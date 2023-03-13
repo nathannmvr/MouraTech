a3d7bcf42c18a0ce596243ef7297661319ceaef8
Nathan Maciel <nathannmvr@gmail.com>
2023-03-13 20:18:17 -0300

    Site login completo

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -1,7 +1,27 @@
 import React from 'react';
+import logo from '../img/logo.png';
 
-function HomePage() {
-  return <h1>Página de Login</h1>;
+function LoginPage() {
+  return (
+    <>
+    <div class = 'min-h-screen flex flex-col justify-center items-center bg-slate-100 flex'>
+      <div>
+        <img src={logo} alt="Logo"/>
+      </div>
+      <h1 class = 'text-3xl my-6'><b>Entre com sua conta</b></h1>
+      <div class = 'text-xl border-2 border-slate-200 rounded-md'>
+        <div>
+          <input type="text" placeholder="E-mail" class = 'border-b-2 border-slate-200' size = '40'/>
+        </div>
+        <div>
+          <input type="text" placeholder="Senha" size = '40'/>
+        </div>
+      </div>
+      <h2 class='indent-80 font-bold text-blue-600 my-4'><a href="">Esqueceu sua senha?</a></h2>
+      <button class = 'flex flex-initial w-1/3  bg-blue-800 text-white justify-center font-bold py-2 rounded'>Entrar</button>
+    </div>
+    </>
+  );
 }
 
-export default HomePage;
+export default LoginPage;
